89e5ef872a251bd804d0a67da3120d50ca7c1329
federico <romerof14@gmail.com>
2022-03-10 15:04:51 -0300

    Terminados los diseños de cada pantalla del modo de juego -moderno-

--- js/clases.js
+++ js/clases.js
@@ -17,3 +17,30 @@ export class Reloj {
 		}
 	}
 }
+
+export class Animacion {
+
+	constructor() {
+		this.styleSheet = null;
+		this.nombre = "";
+	}
+
+	agregarAnimacion(elemento, tiempo_ms, infinita) {
+		elemento.style.animation = this.nombre + ' ' + tiempo + 'ms' + (infinita? ' infinite' : '');
+	}
+
+	crearAnimacionDinamica(nombre, estilos) {
+		
+		if (!this.styleSheet) {
+  			
+  			this.styleSheet = document.createElement('style');
+  			this.styleSheet.type = 'text/css';
+  			document.head.appendChild(this.styleSheet);
+		}
+
+		this.styleSheet.sheet.insertRule(`@keyframes ${nombre} {${estilos}}`,
+		this.styleSheet.length );
+
+		this.nombre = nombre;
+	}
+}
